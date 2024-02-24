d80017e5f74853eccb5251355edd11169c353dfd
Priyank <priyankprajapati126@gmail.com>
2024-02-23 23:56:27 -0500

    Upload Algorithm

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public GameObject boidPrefab;
+    public int totalBoids = 30;
+    public float spawnRadius = 10;
+
+    [Range(1f, 100f)]
+    public float driveFactor = 10f;
+
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+
+    void Start()
+    {
+        for(int i=0; i<totalBoids; i++)
+        {
+            boidPrefab.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.black, Color.white, 6f);
+            Vector3 boidPos = new Vector3(Random.Range(-spawnRadius, spawnRadius), Random.Range(-spawnRadius, spawnRadius));
+            GameObject boid = Instantiate(boidPrefab, this.transform.position + boidPos, Quaternion.identity) as GameObject;
+            boid.transform.parent = this.gameObject.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/FlockBehaviour.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockBehaviour : MonoBehaviour
+{
+    public GameObject manager;
+    public Flock flock;
+    public Vector2 location = Vector2.zero;
+    public Vector2 velocity;
+    
+    private Rigidbody2D boidrBody;
+    private RaycastHit2D[] hits;
+    private Vector2 mousePointer = Vector2.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        boidrBody = this.GetComponent<Rigidbody2D>();
+        manager = transform.parent.gameObject;
+        flock = manager.GetComponent<Flock>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        FLockController();
+        Avoidance();
+        FacingFront();
+        mousePointer = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+    }
+
+    private void FacingFront()
+    {
+        transform.up = boidrBody.velocity;
+    }
+
+    private Vector2 Seek (Vector2 target)
+    {
+        return (target - location);
+    }
+
+    private Vector2 Alignment()
+    {
+        float neighborRadius = flock.neighborRadius;
+        Vector2 sum = Vector2.zero;
+        hits = Physics2D.CircleCastAll(location, neighborRadius, Vector2.zero);
+        int count = 0;
+        foreach(RaycastHit2D hit in hits)
+        {
+            if (hit.transform.gameObject == this.gameObject) continue;
+            if(hit.transform.gameObject.GetComponent<FlockBehaviour>().manager == manager)
+            {
+                count++;
+                sum += hit.transform.GetComponent<Rigidbody2D>().velocity;
+            }
+        }
+
+        if(count > 0)
+        {
+            sum /= count;
+            return (sum - velocity);
+        }
+        else
+        {
+            return sum;
+        }
+    }
+
+    private void Avoidance()
+    {
+        hits = Physics2D.CircleCastAll(location, 1f, Vector2.zero);
+        foreach(RaycastHit2D hit in hits)
+        {
+            if(hit.transform.gameObject.GetComponent<FlockBehaviour>().manager !=  manager)
+            {
+                hit.rigidbody.AddForce(hit.transform.position - this.transform.position);
+            }
+        }
+    }
+
+    private Vector2 Cohesion()
+    {
+        float neighborRadius = flock.neighborRadius;
+        Vector2 sum = Vector2.zero;
+        hits = Physics2D.CircleCastAll(location, neighborRadius, Vector2.zero);
+        int count = 0;
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.transform.gameObject == this.gameObject) continue;
+            if (hit.transform.gameObject.GetComponent<FlockBehaviour>().manager == manager)
+            {
+                count++;
+                sum += hit.transform.GetComponent<Rigidbody2D>().velocity;
+            }
+        }
+
+        if (count > 0)
+        {
+            sum /= count;
+            return Seek(sum);
+        }
+        else
+        {
+            return sum;
+        }
+    }
+
+    private void ApplyForce(Vector2 force)
+    {
+        if(force.magnitude > flock.driveFactor)
+        {
+            force = force.normalized * manager.GetComponent<Flock>().driveFactor;
+        }
+        boidrBody.AddForce(force);
+
+        if(boidrBody.velocity.magnitude > flock.driveFactor)
+        {
+            boidrBody.velocity = boidrBody.velocity.normalized * flock.maxSpeed;
+        }
+
+        if(Seek(mousePointer).magnitude < 1)
+        {
+            boidrBody.drag = 1f;
+        }
+        else
+        {
+            boidrBody.drag = 0;
+        }
+    }
+
+    private void FLockController()
+    {
+        location = this.transform.position;
+        velocity = boidrBody.velocity;
+        Vector2 positionToMove = Seek(mousePointer);
+
+        Vector2 boidMovement = Alignment() + Cohesion() + positionToMove;
+
+        ApplyForce(boidMovement.normalized);
+    }
+}
